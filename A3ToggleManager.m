b08da48cdba3d5d8c36b7ca69d57bfa1aecc168e
Ryan Petrich <rpetrich@gmail.com>
2013-05-20 18:19:07 -0600

    Support drawing shaded and blurred glyphs

--- A3ToggleManager.m
+++ A3ToggleManager.m
@@ -73,6 +73,55 @@ static void TogglesChangedCallback(CFNotificationCenterRef center, void *observe
 	return LMResponseConsumePropertyList(&responseBuffer);
 }
 
+static UIColor *ColorWithHexString(NSString *stringToConvert)
+{
+	NSString *noHashString = [stringToConvert stringByReplacingOccurrencesOfString:@"#" withString:@""]; // remove the #
+	NSScanner *scanner = [NSScanner scannerWithString:noHashString];
+	[scanner setCharactersToBeSkipped:[NSCharacterSet symbolCharacterSet]]; // remove + and $
+
+	unsigned hex;
+	if (![scanner scanHexInt:&hex]) return nil;
+	int r = (hex >> 16) & 0xFF;
+	int g = (hex >> 8) & 0xFF;
+	int b = (hex) & 0xFF;
+
+	return [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:1.0f];
+}
+
+- (void)drawGlyphImageIdentifier:(id)identifier toSize:(CGFloat)glyphSize atPosition:(CGPoint)position color:(CGColorRef)color blur:(CGFloat)blur inContext:(CGContextRef)context ofSize:(CGSize)contextSize
+{
+	CGContextTranslateCTM(context, position.x, position.y);
+	if ([identifier isKindOfClass:[NSString class]]) {
+		UIImage *image;
+		if ([identifier hasSuffix:@".pdf"]) {
+			CGPDFDocumentRef pdf = CGPDFDocumentCreateWithURL((CFURLRef)[NSURL fileURLWithPath:identifier]);
+			if (pdf) {
+				//CGContextSetShadowWithColor(context, CGSizeMake(0.0f, size.height), 0.0f, color);
+				CGContextTranslateCTM(context, 0.0f, -contextSize.height);
+				//CGContextFillRect(context, CGRectMake(0.0f, 0.0f, glyphSize, glyphSize));
+				CGContextScaleCTM(context, 1.0f, -1.0f);
+				CGContextTranslateCTM(context, 0, -glyphSize);
+				CGPDFPageRef firstPage = CGPDFDocumentGetPage(pdf, 1);
+				CGRect rect = CGPDFPageGetBoxRect(firstPage, kCGPDFCropBox);
+				// Shadows are always in the current CTM. whiskey. tango. foxtrot.
+				CGFloat scale = rect.size.height / glyphSize;
+				CGContextSetShadowWithColor(context, CGSizeMake(0.0f, contextSize.height * -scale), blur * scale, color);
+				CGContextScaleCTM(context, glyphSize / rect.size.width, glyphSize / rect.size.height);
+				CGContextTranslateCTM(context, -rect.origin.x, -rect.origin.y);
+				CGContextDrawPDFPage(context, firstPage);
+				CGPDFDocumentRelease(pdf);
+			}
+		} else if ((image = [UIImage imageWithContentsOfFile:identifier])) {
+			identifier = image;
+		}
+	}
+	if ([identifier isKindOfClass:[UIImage class]]) {
+		CGContextSetShadowWithColor(context, CGSizeMake(0.0f, contextSize.height), blur, color);
+		CGContextTranslateCTM(context, 0.0f, -contextSize.height);
+		[identifier drawInRect:CGRectMake(0.0f, 0.0f, glyphSize, glyphSize)];
+	}
+}
+
 - (UIImage *)toggleImageForToggleIdentifier:(NSString *)toggleIdentifier controlState:(UIControlState)controlState scale:(CGFloat)scale usingTemplateBundle:(NSBundle *)template
 {
 	CGSize size;
@@ -83,12 +132,24 @@ static void TogglesChangedCallback(CFNotificationCenterRef center, void *observe
 	if (size.height == 0.0f)
 		return nil;
 	if (&UIGraphicsBeginImageContextWithOptions != NULL) {
-		UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
+		UIGraphicsBeginImageContextWithOptions(size, NO, scale);
 	} else {
 		UIGraphicsBeginImageContext(size);
+		scale = 1.0f;
 	}
+	size_t maskWidth = size.width * scale;
+	size_t maskHeight = size.height * scale;
+	void *maskData = NULL;
+	void *secondMaskData = NULL;
 	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSetFillColorWithColor(context, [UIColor redColor].CGColor);
+	CGContextFillRect(context, CGRectMake(0.0f, 0.0f, size.width, size.height));
 	for (NSDictionary *layer in [template objectForInfoDictionaryKey:@"layers"]) {
+		CGContextSaveGState(context);
+		id temp = [layer objectForKey:@"opacity"];
+		if (temp) {
+			CGContextSetAlpha(context, [temp floatValue]);
+		}
 		CGPoint position = CGPointMake([[layer objectForKey:@"x"] floatValue], [[layer objectForKey:@"y"] floatValue]);
 		NSString *type = [layer objectForKey:@"type"];
 		if (!type || [type isEqualToString:@"image"]) {
@@ -98,35 +159,65 @@ static void TogglesChangedCallback(CFNotificationCenterRef center, void *observe
 				[image drawAtPoint:position];
 			}
 		} else if ([type isEqualToString:@"glyph"]) {
+			CGFloat blur = [[layer objectForKey:@"blur"] floatValue];
 			CGFloat glyphSize = [[layer objectForKey:@"size"] floatValue];
 			id identifier = [self glyphImageIdentifierForToggleIdentifier:toggleIdentifier controlState:controlState size:glyphSize scale:scale];
-			if ([identifier isKindOfClass:[NSString class]]) {
-				UIImage *image;
-				if ([identifier hasSuffix:@".pdf"]) {
-					CGPDFDocumentRef pdf = CGPDFDocumentCreateWithURL((CFURLRef)[NSURL fileURLWithPath:identifier]);
-					if (pdf) {
-						CGPDFPageRef firstPage = CGPDFDocumentGetPage(pdf, 1);
-						CGRect rect = CGPDFPageGetBoxRect(firstPage, kCGPDFCropBox);
-						CGContextScaleCTM(context, 1.0f, -1.0f);
-						CGContextTranslateCTM(context, 0, -size.height);
-						CGContextScaleCTM(context, glyphSize / rect.size.width, glyphSize / rect.size.height);
-						CGContextTranslateCTM(context, -rect.origin.x, -rect.origin.y);
-						CGContextDrawPDFPage(context, firstPage);
-						CGPDFDocumentRelease(pdf);
-					} else {
-						NSLog(@"PDF failed!");
-					}
-				} else if ((image = [UIImage imageWithContentsOfFile:identifier])) {
-					identifier = image;
-				}
+			NSString *fileName = [layer objectForKey:@"fileName"];
+			BOOL hasCutout = [[layer objectForKey:@"cutout"] boolValue];
+			if (hasCutout) {
+				CGFloat cutoutX = [[layer objectForKey:@"cutoutX"] floatValue];
+				CGFloat cutoutY = [[layer objectForKey:@"cutoutY"] floatValue];
+				CGFloat cutoutBlur = [[layer objectForKey:@"cutoutBlur"] floatValue];
+				if (!maskData)
+					maskData = malloc(maskWidth * maskHeight);
+				memset(maskData, '\0', maskWidth * maskHeight);
+				CGContextRef maskContext = CGBitmapContextCreate(maskData, maskWidth, maskHeight, 8, maskWidth, NULL, kCGImageAlphaOnly);
+				CGContextSetBlendMode(maskContext, kCGBlendModeCopy);
+				[self drawGlyphImageIdentifier:identifier toSize:glyphSize * scale atPosition:CGPointMake((position.x + cutoutX) * scale, (position.y + cutoutY) * scale) color:[UIColor whiteColor].CGColor blur:cutoutBlur * scale inContext:maskContext ofSize:CGSizeMake(maskWidth, maskHeight)];
+				CGContextRelease(maskContext);
+				CGDataProviderRef dataProvider = CGDataProviderCreateWithCFData((CFDataRef)[NSData dataWithBytesNoCopy:maskData length:maskWidth * maskHeight freeWhenDone:NO]);
+				CGImageRef maskImage = CGImageMaskCreate(maskWidth, maskHeight, 8, 8, maskWidth, dataProvider, NULL, TRUE);
+				CGDataProviderRelease(dataProvider);
+				CGContextClipToMask(context, CGRectMake(0.0f, 0.0f, size.width, size.height), maskImage);
+				CGImageRelease(maskImage);
 			}
-			if ([identifier isKindOfClass:[UIImage class]]) {
-				[identifier drawInRect:(CGRect){ position, (CGSize){ glyphSize, glyphSize } }];
+			UIImage *image;
+			if (fileName && (image = [UIImage imageNamed:fileName inBundle:template])) {
+				// Slow path to draw an image
+				void *localMaskData;
+				if (hasCutout) {
+					if (!secondMaskData)
+						secondMaskData = malloc(maskWidth * maskHeight);
+					localMaskData = secondMaskData;
+				} else {
+					// Reuse a single buffer if possible
+					if (!maskData)
+						maskData = malloc(maskWidth * maskHeight);
+					localMaskData = maskData;
+				}
+				memset(localMaskData, '\0', maskWidth * maskHeight);
+				CGContextRef maskContext = CGBitmapContextCreate(localMaskData, maskWidth, maskHeight, 8, maskWidth, NULL, kCGImageAlphaOnly);
+				CGContextSetBlendMode(maskContext, kCGBlendModeCopy);
+				[self drawGlyphImageIdentifier:identifier toSize:glyphSize * scale atPosition:CGPointMake(position.x * scale, position.y * scale) color:[UIColor whiteColor].CGColor blur:blur * scale inContext:maskContext ofSize:CGSizeMake(maskWidth, maskHeight)];
+				CGImageRef maskImage = CGBitmapContextCreateImage(maskContext);
+				CGContextRelease(maskContext);
+				CGContextClipToMask(context, CGRectMake(0.0f, 0.0f, size.width, size.height), maskImage);
+				CGImageRelease(maskImage);
+				[image drawInRect:CGRectMake(position.x - blur, position.y - blur, glyphSize + blur + blur, glyphSize + blur + blur)];
+			} else {
+				// Fast path for a solid color
+				CGColorRef color = (ColorWithHexString([layer objectForKey:@"color"]) ?: [UIColor blackColor]).CGColor;
+				[self drawGlyphImageIdentifier:identifier toSize:glyphSize atPosition:position color:color blur:blur inContext:context ofSize:size];
 			}
 		}
+		CGContextRestoreGState(context);
 	}
 	UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
 	UIGraphicsEndImageContext();
+	if (maskData)
+		free(maskData);
+	if (secondMaskData)
+		free(secondMaskData);
 	return result;
 }
 
